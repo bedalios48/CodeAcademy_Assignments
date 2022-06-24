ff58b06ef35faab2634d77fea82c76257b584ac4
bedalios48 <98773788+bedalios48@users.noreply.github.com>
2022-06-24 12:51:55 +0300

    0622

---
+++ Assignments/Assignments/Assignments/AmziausMelagis.cs
@@ -0,0 +1,273 @@
+﻿namespace Assignments06.Assignments
+{
+    internal class AmziausMelagis
+    {
+        public void PateiktiRegistracijosForma()
+        {
+            /*Sukurkite programą, kuri pateiktų vartotojo registracijos formą.
+             Vartotojas įveda:
+              - vardą ir pavardę
+              - asmens kodą (11simb.)
+              - amžių (sveiką skaičių metais) ir/arba gimimo datą (galima abu, tik kažkurį vieną, ar neįvesti nei vieno)
+            Programa į ekraną išveda ataskatą:
+             - šiandienos datą
+             - Vardas, pavardė
+             - Lytis
+                <HINT> asmens kodo pirmasis rodo gimimo šimtmetį ir asmens lytį
+                (1 – XIX a. gimęs vyras,
+                 2 – XIX a. gimusi moteris,
+                 3 – XX a. gimęs vyras,
+                 4 – XX a. gimusi moteris,
+                 5 – XXI a. gimęs vyras,
+                 6 – XXI a. gimusi moteris
+                 tolesni šeši:
+                      metai (du skaitmenys),
+                      mėnuo (du skaitmenys),
+                      diena (du skaitmenys))    
+             - Asmens kodas
+                <NEPRIVALOMAS PASUNKINIMAS> asmens kodas validuojamas pagal tokias salygas
+                   Paskaičiuojamas Kontrolinis skaičius
+                   a) jei kontrolinis skaičius teisingas išvedamas asmens kodas
+                   b) jei neteisingas išvedamas tekstas "kodas neteisingas",
+                      o laukeAmžiaus patikimumas išvedama "patikimumui trūksta duomenų"
+                      <HINT> https://lt.wikipedia.org/wiki/Asmens_kodas
+             - Amžius
+             - Amžiaus patikimumas - pagal tokias sąlygas:
+             -  jei įvestas amžius metais, paskaičiuoti gimimo metus ir sulyginti su įvestu asmens kodu.
+                a) jei sutampa išvesti "amžius patikimas"
+                b) jei nesutampa išvesti "amžius pameluotas"
+             - jei įvesta gimimo data sulyginti su įvestu asmens kodu.
+                a) jei sutampa išvesti "amžius patikimas"
+                b) jei nesutampa išvesti "amžius pameluotas"
+             - jei įvesta ir amžius ir gimimo data sulyginti su įvestu asmens kodu.
+                a) jei viskas sutampa išvesti "amžius tikras"
+                b) jei asmens kodu sutampa tik vienas įvestų, išvesti "amžius nepatikimas"
+                c) jei nesutampa išvesti "amžius pameluotas"
+             - jei neįvesta nei amžius nei gimimo data išvesti
+                a) "patikimumui trūksta duomenų"
+     
+
+                Rezultatas gali atrodyti taip:
+                ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+                ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ATASKAITA APIE ASMENĮ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+                ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓      2022-06-20       ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+                ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+                ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+                ▓     Vardas, pavardė ▓ Vardenis Pavardenis                 ▓
+                ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+                ▓               Lytis ▓ Vyras                               ▓
+                ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+                ▓        Asmens kodas ▓ 44012029286                         ▓
+                ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+                ▓              Amžius ▓ 82                                  ▓
+                ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+                ▓         Gimimo data ▓ 1980-06-20                          ▓
+                ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+                ▓ Amžiaus patikimumas ▓ amžius nepatikimas                  ▓
+                ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+                ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓*/
+
+            Console.Write("Iveskite varda ir pavarde:   ");
+            var vardasPavarde = Console.ReadLine();
+            if (// is vis neivesta:
+                string.IsNullOrEmpty(vardasPavarde) || 
+                // ivestas tik vardas arba tik pavarde:
+                !vardasPavarde.Contains(" "))
+            {
+                Console.WriteLine("Vardas ir pavarde privalomi");
+                Environment.Exit(1);
+            }
+
+            Console.Write("Iveskite asmens koda:   ");
+            var asmensKodas = Console.ReadLine();
+            if (asmensKodas.Length != 11)
+            {
+                Console.WriteLine("Blogas asmens kodas");
+                    Environment.Exit(1);
+            }
+
+            var lytis = GautiLyti(asmensKodas[0]);
+
+            Console.Write("Iveskite amziu ir gimimo data (nebutina):   ");
+            var amziusGimimoData = Console.ReadLine();
+
+            var amzius = GautiAmziu(amziusGimimoData);
+            var gimimoData = GautiGimimoData(amziusGimimoData);
+
+            var amziausPatikimumas = GautiAmziausPatikimuma(amzius, gimimoData, ValiduotiAsmensKoda(asmensKodas));
+
+            AtspausdintiRezultata(DateTime.Now.ToString("yyyy - MM - dd"),
+                vardasPavarde,
+                lytis,
+                ValiduotiAsmensKoda(asmensKodas),
+                amzius.ToString(),
+                gimimoData?.ToString("yyyy - MM - dd"),
+                amziausPatikimumas);
+        }
+
+        private string GautiLyti(char asmensKodoPirmasSkaitmuo)
+        {
+            var skaitmuo = int.Parse(asmensKodoPirmasSkaitmuo.ToString());
+            var liekana = skaitmuo % 2;
+            return liekana switch
+            {
+                0 => "Moteris",
+                1 => "Vyras",
+                _ => ""
+            };
+        }
+
+        private static int? GautiAmziu(string amziusGimimoData)
+        {
+            var reiksme = amziusGimimoData.Contains(" ") ?
+                amziusGimimoData.Substring(0, amziusGimimoData.IndexOf(" ")) :
+                amziusGimimoData;
+            var arAmzius = int.TryParse(reiksme, out var amzius);
+            if (arAmzius)
+                return amzius;
+
+            if (!amziusGimimoData.Contains(" "))
+                return null;
+
+            reiksme = amziusGimimoData.Substring(amziusGimimoData.IndexOf(" "), amziusGimimoData.Length - amziusGimimoData.IndexOf(" "));
+            arAmzius = int.TryParse(reiksme, out amzius);
+            if (arAmzius)
+                return amzius;
+            
+            return null;
+        }
+
+        private DateTime? GautiGimimoData(string amziusGimimoData)
+        {
+            var reiksme = amziusGimimoData.Contains(" ") ?
+                amziusGimimoData.Substring(0, amziusGimimoData.IndexOf(" ")) :
+                amziusGimimoData;
+            var arGimimoData = DateTime.TryParse(reiksme, out var gimimoData);
+            if (arGimimoData)
+                return gimimoData;
+
+            if (!amziusGimimoData.Contains(" "))
+                return null;
+
+            reiksme = amziusGimimoData.Substring(amziusGimimoData.IndexOf(" "), amziusGimimoData.Length - amziusGimimoData.IndexOf(" "));
+            arGimimoData = DateTime.TryParse(reiksme, out gimimoData);
+            if (arGimimoData)
+                return gimimoData;
+
+            return null;
+        }
+
+        private string GautiAmziausPatikimuma(int? amzius, DateTime? gimimoData, string asmensKodas)
+        {
+            if (asmensKodas == "kodas neteisingas")
+                return "patikimumui trūksta duomenų";
+
+            if (amzius == null && gimimoData == null)
+                return "patikimumui trūksta duomenų";
+
+            var gimimoDataIsAsmensKodo = DateTime.Parse(GautiDataIsAsmensKodo(asmensKodas));
+            var amziusIsAsmensKodo = (DateTime.Now - gimimoDataIsAsmensKodo).Days / 365;
+            if (gimimoData == null)
+            {
+                if (amzius == amziusIsAsmensKodo)
+                    return "amžius patikimas";
+                return "amžius pameluotas";
+            }
+
+            if (amzius == null)
+            {
+                if (gimimoDataIsAsmensKodo == gimimoData)
+                    return "amžius patikimas";
+                return "amžius pameluotas";
+            }
+
+            if (gimimoDataIsAsmensKodo == gimimoData)
+            {
+                if (amzius == amziusIsAsmensKodo)
+                    return "amžius tikras";
+                return "amžius nepatikimas";
+            }
+
+            if (amzius == amziusIsAsmensKodo)
+                return "amžius nepatikimas";
+            return "amžius pameluotas";
+        }
+
+        private string ValiduotiAsmensKoda(string asmensKodas)
+        {
+            var arTeisingaData = DateTime.TryParse(GautiDataIsAsmensKodo(asmensKodas), out _);
+            if (!arTeisingaData)
+                return "kodas neteisingas";
+
+            var kontrolinisSkaicius = (int.Parse(asmensKodas[0].ToString()) * 1 + int.Parse(asmensKodas[1].ToString()) * 2 +
+                int.Parse(asmensKodas[2].ToString()) * 3 + int.Parse(asmensKodas[3].ToString()) * 4 + int.Parse(asmensKodas[4].ToString()) * 5 +
+                int.Parse(asmensKodas[5].ToString()) * 6 + int.Parse(asmensKodas[6].ToString()) * 7 + int.Parse(asmensKodas[7].ToString()) * 8 +
+                int.Parse(asmensKodas[8].ToString()) * 9 + int.Parse(asmensKodas[9].ToString()) * 1) % 11;
+            if (kontrolinisSkaicius == 10)
+            {
+                kontrolinisSkaicius = (int.Parse(asmensKodas[0].ToString()) * 3 + int.Parse(asmensKodas[1].ToString()) * 4 +
+                int.Parse(asmensKodas[2].ToString()) * 5 + int.Parse(asmensKodas[3].ToString()) * 6 + int.Parse(asmensKodas[4].ToString()) * 7 +
+                int.Parse(asmensKodas[5].ToString()) * 8 + int.Parse(asmensKodas[6].ToString()) * 9 + int.Parse(asmensKodas[7].ToString()) * 1 +
+                int.Parse(asmensKodas[8].ToString()) * 2 + int.Parse(asmensKodas[9].ToString()) * 3) % 11;
+            }
+
+            if (kontrolinisSkaicius == 10)
+                kontrolinisSkaicius = 0;
+
+            if (int.Parse(asmensKodas[10].ToString()) != kontrolinisSkaicius)
+                return "kodas neteisingas";
+
+            return asmensKodas;
+        }
+
+        private string GautiDataIsAsmensKodo(string asmensKodas)
+        {
+            var pirmiDuMetuSkaitmenys = GautiPirmusDuMetuSkaitmenis(asmensKodas[0]);
+            var metai = pirmiDuMetuSkaitmenys + asmensKodas.Substring(1, 2);
+            var menuo = asmensKodas.Substring(3, 2);
+            var diena = asmensKodas.Substring(5, 2);
+            return $"{metai}-{menuo}-{diena}";
+        }
+
+        private string GautiPirmusDuMetuSkaitmenis(char asmensKodoPirmasSkaitmuo)
+        {
+            var skaitmuo = int.Parse(asmensKodoPirmasSkaitmuo.ToString());
+            return skaitmuo switch
+            {
+                1 => "18",
+                2 => "18",
+                3 => "19",
+                4 => "19",
+                5 => "20",
+                6 => "20",
+                _ => ""
+            };
+        }
+
+        private void AtspausdintiRezultata(string ataskaitosData, string vardasPavarde, string lytis, string asmensKodas,
+            string amzius, string gimimoData, string amziausPatikimumas)
+        {
+            var eileBeTeksto = "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓" + Environment.NewLine;
+            var ataskaitosEile = "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ATASKAITA APIE ASMENĮ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓" + Environment.NewLine
+                + "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     " + ataskaitosData + "    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓" + Environment.NewLine;
+            var vardasPavardeEile = "▓     Vardas, pavardė ▓ ";
+            var eilesGalas = "▓" + Environment.NewLine;
+            var lytisEile = "▓               Lytis ▓ ";
+            var asmensKodasEile = "▓        Asmens kodas ▓ ";
+            var amziusEile = "▓              Amžius ▓ ";
+            var gimimoDataEile = "▓         Gimimo data ▓ ";
+            var amziausPatikimumasEile = "▓ Amžiaus patikimumas ▓ ";
+
+            Console.Write("{0}{1}{0}{0}" +
+                "{2}{3,-36}{4}{0}" +
+                "{5}{6,-36}{4}{0}" +
+                "{7}{8,-36}{4}{0}" +
+                "{9}{10,-36}{4}{0}" +
+                "{11}{12,-36}{4}{0}" +
+                "{13}{14,-36}{4}{0}{0}", 
+                eileBeTeksto, ataskaitosEile, vardasPavardeEile, vardasPavarde, eilesGalas,
+                lytisEile, lytis, asmensKodasEile, asmensKodas, amziusEile, amzius, gimimoDataEile, gimimoData,
+                amziausPatikimumasEile, amziausPatikimumas);
+        }
+    }
+}
--- Assignments/Assignments/Program.cs
+++ Assignments/Assignments/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Assignments
+﻿using Assignments06.Assignments;
+
+namespace Assignments06
 {
     internal class Program
     {
@@ -7,154 +9,17 @@
             Console.WriteLine("Hello, World!");
 
             /*
-             Paprašykite naudotojo įvesti 1 skaičių - temperatūrą pagal Celsijų. 
-               - Paskaičiuokite ir išveskite į ekraną temperatūrą pagal farenheitą.
-               - Paskaičiuokite ir išveskite į ekraną temperatūrą pagal kelviną.
-               - Gautą temperatūrą pagal farenheitą perskaičiuokite į Celsijų ir patikrinkite ar sutampa su įvestu skaičių (išveskite true/false)
-               - Gautą temperatūrą pagal kelviną perskaičiuokite į celsijų ir patikrinkite ar sutampa su įvestu skaičiu (išveskite true/false)
-               - Paskaičiuotą temperatūrą pagal farenheita peverskite į kelviną ir patikrinkite ar sutampa su ankstesniais skaičiavimais (išveskite true/false)
-               - Nupieškite termometrą pagal Celsijų 
-                 a) Atvaizduokite skalę, sugraduotą kas 5 laipsnius C priklausomai nuo įvestos temperatūros pridedant ir atimant 40 laipsnių 
-                   (tarkime įvesta buvo 10, tuomet skalė bus nuo -30 iki +50)
-                 b) Grafiškai atvaizduokite įvestą temperatūros stulpelį. 
-                    <HINT> naudokite .ToString(), palyginimo reliacinius operatorius (==, >, < ir t.t.) ir .Replace(). 
-                    if naudoti negalima, ternary operator (?) naudoti negalima.
-            rezultatas gali atrodyti taip:
-                                        |--------------------|
-                                        |   ^F     _    ^C   |
-                                        |  100  - | | -  40  |
-                                        |   95  - | | -  35  |
-                                        |   90  - | | -  30  |
-                                        |   80  - | | -  25  |
-                                        |   70  - | | -  20  |
-                                        |   60  - | | -  15  |
-                                        |   50  - |#| -  10  |
-                                        |   40  - |#| -   5  |
-                                        |   30  - |#| -   0  |
-                                        |   20  - |#| -  -5  |
-                                        |   10  - |#| - -10  |
-                                        |    5  - |#| - -15  |
-                                        |    0  - |#| - -20  |
-                                        |  -10  - |#| - -25  |
-                                        |  -20  - |#| - -30  |
-                                        |  -30  - |#| - -35  |
-                                        |  -40  - |#| - -40  |
-                                        |        '***`       |
-                                        |       (*****)      |
-                                        |        `---'       |
-                                        |____________________|
-             */
-            /*
-            Console.Write("Iveskite temperatura celsijais:  ");
-            var temperaturaCelsijais = double.Parse(Console.ReadLine());
-            var temperaturaFarenheitais = temperaturaCelsijais * 9.0 / 5 + 32;
-            Console.WriteLine("Temperatura Farenheitais: {0}", temperaturaFarenheitais);
-            var temperaturaKelvinais = temperaturaCelsijais + 273.15;
-            Console.WriteLine("Temperatura Kelvinais: {0}", temperaturaKelvinais);
-            var temperaturaCelsijais2 = (temperaturaFarenheitais - 32) / 1.8;
-            Console.WriteLine("Ar {0} sutampa su {1}? Atsakymas: {2}",
-                temperaturaCelsijais, temperaturaCelsijais2, temperaturaCelsijais == temperaturaCelsijais2);
-            var temperaturaCelsijais3 = temperaturaKelvinais - 273.15;
-            Console.WriteLine("Ar {0} sutampa su {1}? Atsakymas: {2}",
-                temperaturaCelsijais, temperaturaCelsijais3, temperaturaCelsijais == temperaturaCelsijais3);
-            var temperaturaKelvinais2 = (temperaturaFarenheitais - 32) / 1.8 + 273.15;
-            Console.WriteLine("Ar {0} sutampa su {1}? Atsakymas: {2}",
-                temperaturaKelvinais, temperaturaKelvinais2, temperaturaKelvinais == temperaturaKelvinais2);
-            Console.WriteLine();
-
-            int gradavimas = (int)temperaturaCelsijais - (int)(temperaturaCelsijais % 5) + (int)((temperaturaCelsijais % 5)/5+0.5) * 5;
-            int skaleMax = gradavimas + 40;
-            int skaleMin = gradavimas - 40;
-            var i = skaleMax;
-            Console.WriteLine("|---------------------|");
-            Console.WriteLine("|    ^F    _    ^C    |");
-            while (i > skaleMin)
-            {
-                var tempF = i * 9.0 / 5 + 32;
-                var a = $"      {tempF} - ";
-                var b = $" - {i}      ";
-                var rodmuoF = a.Substring(a.Length - 10, 9);
-                var rodmuoC = b.Substring(0, 9);
-                var stulpelis = (gradavimas >= i).ToString()[0].ToString().Replace('T', '#').Replace('F', ' ');
-                Console.WriteLine("|{0}|{2}|{1}|", rodmuoF, rodmuoC, stulpelis);
-                i -= 5;
-            }
-            Console.WriteLine("|        '***`        |");
-            Console.WriteLine("|       (*****)       |");
-            Console.WriteLine("|        `---'        |");
-            Console.WriteLine("|_____________________|");
+            var assignment1 = new Assignment1();
+            assignment1.Method();
             */
 
             /*
-             PARAŠYTI PROGRAMĄ KURI PRAŠO ĮVESTI ATSTUMĄ (KILOMENTRAIS) TARP TAŠKŲ A IR B IR DVIEJŲ TRANSPORTO PRIEMONIŲ GREITĮ (KM/H). 
-             VIENA TRANSPORTO PRIEMONĖS PRADEDA VAŽIUOTI IŠ A, KITA IŠ B.STARTUOJA VIENU METU.
-              - PASKAIČIUOTI ATSTUMĄ NUO A IKI VIETOS KURIOJE TRASPORTO PRIEMONĖS SUTITIKS METRAIS. 
-              - PASKAIČIUOTI LAIKĄ KADA TRASPORTO PRIEMONĖS SUSITIKS SEKUNDĖMIS. 
-              - PASKAIČIUOTI LAIKĄ, KADA TRASPORTUO PRIEMONĖS PASIEKS GALIUTINIUS TAŠKUS MINUTĖMIS.
-              - PASKAIČIUOTI KIEK GRAMŲ CO2 IŠSKYRĖ ABI TRASPORTO PIEMONĖS KARTU SUDĖJUS. CO2 NORMA YRA 95 g/km.
-              - GRAFIŠKAI PAVAIZDUOTI KELIĄ NUO A IKI B SUSKIRSTYTĄ Į 20 LYGIŲ SEGMENTŲ (TARKIME ĮVESTAS ATSTUMAS YRA 100KM, TUOMENT TURĖSIME 20 SEGMENTU PO 5KM).  
-                A) PARODYTI VISO KELIO ILGĮ KM
-                B) PARODYTI SEGMENTO ILGĮ KM
-                C) PARODYTI KURIAME SEGMENTE TRASPORTO PREMONĖS SUTIKS IR ATSTUMĄ IKI SUSITIKIMO (TAŠKAS V)
-                D) PARODYTI ABIEJŲ TRANSPORTO PRIEMONIŲ VAŽIAVIMO TRUKMĘ
-                REZULTATAS GALI ARTODYTI TAIP:
-               viso 100 km
-             |--------------------------------------------------------------------------------------------------------------------------------------------|
-             0      5     10     15     20      25     30     35     40     45     50     55     60     65     70     75     80     85     90     95    100
-             |      |      |      |      |       |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
-            _A______|______|______|______|___V___|______|______|______|______|______|______|______|______|______|______|______|______|______|______|______B_
-             |-------------------------------|                                                                                                                                                        
-               susitikimo vieta 23,1 km
-               susitikimo laikas po 0,87 val.
-             | >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   200 min >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>|
-             |<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   60 min <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< |
-             */
+            var assignment2 = new Assignment2();
+            assignment2.Method();
+            */
 
-            // default: km, h, g
-
-            Console.Write("Iveskite atstuma tarp A ir B (km):    ");
-            var atstumas = int.Parse(Console.ReadLine());
-            Console.Write("Iveskite A priemones greiti (km/h):  ");
-            var greitisA = int.Parse(Console.ReadLine());
-            Console.Write("Iveskite B priemones greiti (km/h):  ");
-            var greitisB = int.Parse(Console.ReadLine());
-            var susitikimoLaikas = (double)atstumas / (greitisA + greitisB);
-            var susitikimoVieta = greitisA * susitikimoLaikas;
-            var minValandoje = 60;
-            var laikasAMin = (double)atstumas / greitisA * minValandoje;
-            var laikasBMin = (double)atstumas / greitisB * minValandoje;
-            var cO2Norma = 92; //g/km
-            var bendrasCO2 = atstumas * cO2Norma * 2;
-            var segmentas = (double)atstumas / 20;
-            var atkarpa = segmentas;
-            Console.WriteLine(" viso {0} km", atstumas);
-            Console.WriteLine(" bendras CO2 {0} g", bendrasCO2);
-            var virsutineLinija = " |----------------------------------------------------------------------" +
-                "---------------------------------------------------------------------|";
-            Console.WriteLine(virsutineLinija);
-            Console.WriteLine(" 0    {0,3:0}    {1,3:0}    {2,3:0}    {3,3:0}    {4,3:0}    {5,3:0}    {6,3:0}    " +
-                "{7,3:0}    {8,3:0}    {9,3:0}    {10,3:0}    {11,3:0}    {12,3:0}    {13,3:0}    {14,3:0}    {15,3:0}    " +
-                "{16,3:0}    {17,3:0}    {18,3:0}    {19,3:0}", atkarpa, atkarpa += segmentas, atkarpa += segmentas, atkarpa += segmentas
-                , atkarpa += segmentas, atkarpa += segmentas, atkarpa += segmentas, atkarpa += segmentas, atkarpa += segmentas, atkarpa += segmentas
-                , atkarpa += segmentas, atkarpa += segmentas, atkarpa += segmentas, atkarpa += segmentas, atkarpa += segmentas, atkarpa += segmentas
-                , atkarpa += segmentas, atkarpa += segmentas, atkarpa += segmentas, atkarpa += segmentas);
-            Console.WriteLine(" |      |      |      |      |      |      |      |      |      |      |      " +
-                "|      |      |      |      |      |      |      |      |      |");
-            var susitikimoVietosLinija = "_A______|______|______|______|______|______|______|______|______|______|______" +
-                "|______|______|______|______|______|______|______|______|______B_";
-            var ilgis = susitikimoVietosLinija.Length - 2;
-            var susitikimoTaskas = (int)(susitikimoVieta / atstumas * ilgis);
-            susitikimoVietosLinija = susitikimoVietosLinija.Substring(0, susitikimoTaskas + 1) + "V"
-                + susitikimoVietosLinija.Substring(susitikimoTaskas + 2, ilgis - susitikimoTaskas);
-            Console.WriteLine(susitikimoVietosLinija);
-            var susitikimoVietosLinija2 = virsutineLinija.Substring(0, susitikimoTaskas + 1) + "|";
-            Console.WriteLine(susitikimoVietosLinija2);
-            Console.WriteLine("   susitikimo vieta {0:0.0} km", susitikimoVieta);
-            Console.WriteLine("   susitikimo laikas po {0:0.00} val.", susitikimoLaikas);
-            Console.WriteLine(" | >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   {0,3:0} min >>>>>>>>>>>>>>" +
-                ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>|", laikasAMin);
-            Console.WriteLine(" |<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  {0,3:0} min <<<<<<<<<<<<<<<<" +
-                "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< |", laikasBMin);
+            var amziausMelagis = new AmziausMelagis();
+            amziausMelagis.PateiktiRegistracijosForma();
         }
     }
 }
