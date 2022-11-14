016a57db0776503a35189a47430dc4bebbcb7c08
bedalios48 <98773788+bedalios48@users.noreply.github.com>
2022-11-14 19:16:40 +0200

    init

---
+++ Assignments_JavaScript/assignment_1115/js/script.js
@@ -0,0 +1,5 @@
+login.addEventListener('click', () => window.location='login-form/index.html')
+
+register.addEventListener('click', function () {
+    window.location='registration-form/index.html'
+})
