fdf864529d2ade5eebfa2dd54fbfd140d9e27c68
bedalios48 <98773788+bedalios48@users.noreply.github.com>
2022-11-14 19:36:13 +0200

    Revert "init"
    This reverts commit 016a57db0776503a35189a47430dc4bebbcb7c08.

--- Assignments_JavaScript/assignment_1115/js/script.js
+++
@@ -1,5 +0,0 @@
-login.addEventListener('click', () => window.location='login-form/index.html')
-
-register.addEventListener('click', function () {
-    window.location='registration-form/index.html'
-})
