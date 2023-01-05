89f93a747a01f71144734aaed77a4a64324e601f
bedalios48 <98773788+bedalios48@users.noreply.github.com>
2023-01-05 22:04:29 +0200

    Services created

---
+++ FinalAssignment/GenealogyTree/GenealogyTree.Infrastructure/Services/JwtService.cs
@@ -0,0 +1,12 @@
+﻿using GenealogyTree.Domain.Interfaces;
+
+namespace GenealogyTree.Infrastructure.Services
+{
+    public class JwtService : IJwtService
+    {
+        public string GetJwtToken(int id, string role)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FinalAssignment/GenealogyTree/GenealogyTree.Infrastructure/Services/PasswordService.cs
@@ -0,0 +1,13 @@
+﻿using GenealogyTree.Domain.Interfaces;
+
+namespace GenealogyTree.Infrastructure.Services
+{
+    public class PasswordService : IPasswordService
+    {
+        public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            passwordHash = new byte[32];
+            passwordSalt = new byte[33];
+        }
+    }
+}
