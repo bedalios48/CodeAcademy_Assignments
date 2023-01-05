9bd47c0705a38d1434d1938b99268cbd9d0ae247
bedalios48 <98773788+bedalios48@users.noreply.github.com>
2023-01-05 22:05:28 +0200

    Automapper

---
+++ FinalAssignment/GenealogyTree/GenealogyTree/Adapters/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using GenealogyTree.Domain.Models;
+using GenealogyTree.DTO;
+
+namespace GenealogyTree.Adapters
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<RegisterRequest, User>();
+        }
+    }
+}
