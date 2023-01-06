ca05ba884e66afb8f2aadc0515f09689ffd4837e
bedalios48 <98773788+bedalios48@users.noreply.github.com>
2023-01-06 16:04:18 +0200

    Password service not implemented

--- FinalAssignment/GenealogyTree/GenealogyTree.Infrastructure/Services/PasswordService.cs
+++ FinalAssignment/GenealogyTree/GenealogyTree.Infrastructure/Services/PasswordService.cs
@@ -6,8 +6,7 @@ namespace GenealogyTree.Infrastructure.Services
     {
         public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
-            passwordHash = new byte[32];
-            passwordSalt = new byte[33];
+            throw new NotImplementedException();
         }
     }
 }
