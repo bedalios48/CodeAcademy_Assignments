7534523de607ef7a2e140c7a8b62eaae57afd7e3
bedalios48 <98773788+bedalios48@users.noreply.github.com>
2023-01-09 19:39:54 +0200

    Create entry returns Id

--- FinalAssignment/GenealogyTree/GenealogyTree.Domain/Interfaces/IRepositories/IRepository.cs
+++ FinalAssignment/GenealogyTree/GenealogyTree.Domain/Interfaces/IRepositories/IRepository.cs
@@ -8,7 +8,7 @@ namespace GenealogyTree.Domain.Interfaces.IRepositories
         Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>>? filter = null);
         Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter, bool tracked = true);
         Task<bool> ExistAsync(Expression<Func<TEntity, bool>> filter);
-        Task CreateAsync(TEntity entity);
+        Task<int> CreateAsync(TEntity entity);
         Task RemoveAsync(TEntity entity);
         Task SaveAsync();
     }
